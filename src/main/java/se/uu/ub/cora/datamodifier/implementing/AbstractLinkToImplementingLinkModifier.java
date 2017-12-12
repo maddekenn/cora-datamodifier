7b9231df0d5bc02671a4a5b637e02ad7adabc447
madde <madde@4fad262a5944>
2017-12-12 20:19:59 +0000

    CORA-463, creating new class, no real work

---
+++ src/main/java/se/uu/ub/cora/datamodifier/implementing/AbstractLinkToImplementingLinkModifier.java
@@ -0,0 +1,5 @@
+package se.uu.ub.cora.datamodifier.implementing;
+
+public class AbstractLinkToImplementingLinkModifier {
+
+}
---
+++ src/test/java/se/uu/ub/cora/datamodifier/implementing/AbstractLinkToImplementingLinkModifierTest.java
@@ -0,0 +1,12 @@
+package se.uu.ub.cora.datamodifier.implementing;
+
+import org.testng.annotations.Test;
+
+public class AbstractLinkToImplementingLinkModifierTest {
+
+	@Test
+	public void init() {
+		AbstractLinkToImplementingLinkModifier modifier = new AbstractLinkToImplementingLinkModifier();
+	}
+
+}
